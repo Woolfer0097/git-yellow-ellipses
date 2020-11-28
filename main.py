ddbcb282f5281675cd8c2b539fa39315f3196d4b
Woolfer0097 <Woolfer0097@yandex.ru>
2020-11-29 00:38:43 +0300

    Second commit, with random color

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import sys
 
 from random import randint
-from PyQt5 import uic
-from PyQt5.QtGui import QPainter, QColor, QBrush
+from PyQt5 import uic, QtCore, QtWidgets
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
+from UI import Ui_MainWindow
 
 
-class YellowEllipses(QMainWindow):
+class YellowEllipses(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.painter = QPainter(self)
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.initUI()
 
@@ -32,7 +33,7 @@ class YellowEllipses(QMainWindow):
 
     def generate(self, qp):
         x, y = randint(0, 500), randint(0, 500)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         qp.drawEllipse(self.ellipse_label.x(), self.ellipse_label.y(), x, y)
         self.do_paint = False
 
